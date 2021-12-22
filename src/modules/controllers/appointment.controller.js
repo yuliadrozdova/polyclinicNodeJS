0bd174144c7873a9efab40e08cfb66438f908df8
yuliadrozdova <drozdova.exceedteam@gmail.com>
2021-12-22 13:13:20 +0300

    refactor routes

--- src/modules/controllers/appointment.controller.js
+++ src/modules/controllers/appointment.controller.js
@@ -11,39 +11,42 @@ module.exports.getAllTricks = async (req, res, next) => {
 }
 
 //create Trick +
-module.exports.createTrick = (req, res, next) => {
-    const {namePatient, nameDoctor, date, textComplaints} = req.body;
-    const newTrick = new Trick({namePatient, nameDoctor, date, textComplaints});
-    newTrick.save().then(result => {
-        res.send({data: result});
-    }).catch(err => console.log(err))
+module.exports.createTrick = async (req, res, next) => {
+    try{
+        const {namePatient, nameDoctor, date, textComplaints} = req.body;
+        const newTrick = new Trick({namePatient, nameDoctor, date, textComplaints});
+        const result = await newTrick.save();
+            res.send({data: result});
+            console.log('111 create ', {data: result});
+    }catch (e) {
+        res.statusCode(500).send(e);
+    }
 }
 
+
 //delete trick +
-module.exports.deleteTrick = (req, res, next) => {
-    const deleteTrick = Trick.deleteOne({_id: req.params.id});
-    deleteTrick.then(result => {
+module.exports.deleteTrick = async (req, res, next) => {
+    try{
+        const deleteTrick = Trick.deleteOne({_id: req.params.id});
+        const result = await deleteTrick;
         res.send({data: result});
-    }).catch(err => console.log(err))
+    }catch (e) {
+        res.statusCode(500).send(e);
+    }
 }
 
 //update Trick -
 module.exports.updateTrick = async (req, res, next) => {
-    const params = req.body;
+    const params = req.body.values;
     console.log('params ', params);
 
     try {
-        const updateTrick = await Trick.updateOne({
+        const updateTrick = await Trick.updateOne({_id : params._id},
+            {$set: {
                 namePatient: params.namePatient,
                 nameDoctor: params.nameDoctor,
                 date: params.date,
-                textComplaints: params.textComplaints
-            },
-            {
-                namePatient: params.updateNamePatient,
-                nameDoctor: params.updateNameDoctor,
-                date: params.updateDate,
-                textComplaints: params.updateTextComplaints
+                textComplaints: params.textComplaints}
             });
 
         res.send(updateTrick);
